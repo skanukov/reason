e35f447e68e46416b80969ba5fa71a1020b341cc
Sergey Kanukov <s.kanukov@gmail.com>
2016-08-12 20:06:39 +0300

    Add logging example

--- src/main/java/com/github/skanukov/sparklet/config/Router.java
+++ src/main/java/com/github/skanukov/sparklet/config/Router.java
@@ -1,15 +1,22 @@
 package com.github.skanukov.sparklet.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static spark.Spark.get;
 
 /**
  * Handles routes for application.
  */
 public final class Router {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     /**
      * Creates router.
      */
     public Router() {
+        logger.info("Router created.");
     }
 
     /**
