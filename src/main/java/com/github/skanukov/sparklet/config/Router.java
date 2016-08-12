b2e6a35684fb6ddcd81580c5eb3c4cb924458acc
Sergey Kanukov <s.kanukov@gmail.com>
2016-08-12 21:07:19 +0300

    Use Home controller

--- src/main/java/com/github/skanukov/sparklet/config/Router.java
+++ src/main/java/com/github/skanukov/sparklet/config/Router.java
@@ -1,7 +1,6 @@
 package com.github.skanukov.sparklet.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.skanukov.sparklet.app.controllers.HomeController;
 
 import static spark.Spark.get;
 
@@ -9,23 +8,14 @@ import static spark.Spark.get;
  * Handles routes for application.
  */
 public final class Router {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    /**
-     * Creates router.
-     */
-    public Router() {
-        logger.info("Router created.");
-    }
-
     /**
      * Defines routes for application.
      *
      * @return Router for fluent interface.
      */
     public Router route() {
-        get("/hello", (req, res) -> "Hello World");
+        get("/", new HomeController()::index);
+        get("/hello", new HomeController()::index);
         return this;
     }
 }
