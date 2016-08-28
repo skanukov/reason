54047d25328a4206937e74579c0b49e32427ecac
Sergey Kanukov <s.kanukov@gmail.com>
2016-08-28 17:50:22 +0300

    Hash assets for production

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,21 @@ const NODE_ENV = process.env.NODE_ENV || 'development',
     isDevelopment = 'development' == NODE_ENV,
     isProduction = !isDevelopment;
 
-const autoprefixer = require('autoprefixer'),
+const AssetsPlugin = require('assets-webpack-plugin'),
+    autoprefixer = require('autoprefixer'),
     ExtractTextPlugin = require('extract-text-webpack-plugin'),
     path = require('path'),
     postcssFlexbugsFixes = require('postcss-flexbugs-fixes'),
     webpack = require('webpack');
 
-// Minify CSS for production
+// Use PostCSS and disable source maps for production
 const sassLoader = 'css-loader' + (isDevelopment ? '?sourceMap' : '!postcss-loader') +
     '!sass-loader' + (isDevelopment ? '?sourceMap' : '');
+
+// Add hash to asset name for production
+function getAssetName() {
+    return '[name]' + (isProduction ? '-[hash]' : '');
+}
 
 module.exports = {
     context: path.resolve(__dirname, './assets'),
@@ -21,7 +27,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './public/assets'),
-        filename: '[name].js'
+        filename: `${getAssetName()}.js`
     },
 
     devtool: isDevelopment ? '#cheap-module-inline-source-map' : null,
@@ -36,17 +42,21 @@ module.exports = {
             // Copy images, fonts, etc.
             {
                 test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png$|\.jpe?g$|\.gif$/,
-                loader: 'file-loader?name=[name]-[hash].[ext]'
+                loader: `file-loader?name=${getAssetName()}.[ext]`
             }
         ]
     },
 
     plugins: [
-        // Extract CSS to separate file
-        new ExtractTextPlugin('[name].css'),
         // Provide jQuery globally
         new webpack.ProvidePlugin({
             jQuery: 'jquery'
+        }),
+        // Extract CSS to separate file
+        new ExtractTextPlugin(`${getAssetName()}.css`),
+        // Create manifest file
+        new AssetsPlugin({
+            path: path.resolve(__dirname, './public/assets')
         })
     ],
 
@@ -61,7 +71,7 @@ module.exports = {
     watch: isDevelopment
 };
 
-// Minify JS in production mode
+// Minify assets for production
 if (isProduction) {
     module.exports.plugins.push(
         new webpack.optimize.UglifyJsPlugin({
