1411c4d862fd0b60347462f004d01cea88b1b7ea
Sergey Kanukov <s.kanukov@gmail.com>
2016-09-11 14:30:05 +0300

    Add Sql2o to query database

---
+++ src/main/java/com/github/skanukov/sparklet/lib/model/User.java
@@ -0,0 +1,39 @@
+package com.github.skanukov.sparklet.lib.model;
+
+import com.github.skanukov.sparklet.core.db.Sql2oFactory;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * User model.
+ */
+public class User {
+    private Long id;
+    private String email;
+    private String passwordDigest;
+    private String rememberToken;
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static List<User> getAll() {
+        Sql2o db = Sql2oFactory.getSql2o();
+        String sql = "SELECT * FROM users";
+
+        List<User> result = null;
+        try (Connection con = db.open()) {
+            result = con.createQuery(sql)
+                    .addColumnMapping("password_digest", "passwordDigest")
+                    .addColumnMapping("remember_token", "rememberToken")
+                    .addColumnMapping("created_at", "createdAt")
+                    .addColumnMapping("updated_at", "updatedAt")
+                    .executeAndFetch(User.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
