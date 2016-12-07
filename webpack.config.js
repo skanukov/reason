cfa8a47d6de23d951f30a2547b2a2d053fceb624
Sergey Kanukov <s.kanukov@gmail.com>
2016-12-07 15:24:24 +0300

    Minify assets in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,57 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development',
+  isDevelopment = 'development' == NODE_ENV,
+  isProduction = !isDevelopment;
+
 const ExtractTextPlugin = require('extract-text-webpack-plugin'),
-  path = require('path');
+  path = require('path'),
+  webpack = require('webpack');
+
+// Add hash to asset name for production.
+function getAssetName() {
+  return '[name]' + (isProduction ? '-[hash]' : '');
+}
 
 module.exports = {
   context: path.join(__dirname, './assets'),
   entry: {
-    bundle: './js/app.js',
+    bundle: './js/app.js'
   },
   output: {
     path: path.join(__dirname, './public/assets'),
-    filename: '[name].js'
+    filename: `${getAssetName()}.js`
   },
+
+  devtool: isDevelopment ? '#cheap-module-inline-source-map' : '#cheap-module-source-map',
 
   module: {
     rules: [{
       test: /\.(sass|scss)$/,
       loader: ExtractTextPlugin.extract({
         fallbackLoader: 'style-loader',
-        loader: 'css-loader!sass-loader'
+        loader: 'css-loader?sourceMap!sass-loader?sourceMap'
       })
     }]
   },
 
   plugins: [
     // Extract CSS to separate file.
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin(`${getAssetName()}.css`)
   ]
 };
+
+// Minify assets for production
+if (isProduction) {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false,
+      },
+      sourceMap: true
+    })
+  );
+}
